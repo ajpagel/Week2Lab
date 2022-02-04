4e0654fb37b396cf3b5a7e143dd8605b0e0e6e81
Amanda Pagel <98364239+ajpagel@users.noreply.github.com>
2022-02-04 16:35:43 -0600

    Update ConditionsAndLoops.js

--- ConditionsAndLoops.js
+++ ConditionsAndLoops.js
@@ -117,39 +117,3 @@ console.log(studentClass)
 
 
 
-
-
-
-// write code that will set the value of studentClass based on studentGrade
-// studentGrade will be K-12
-// K-6 will be Elementary
-// 7-8 will be Middle
-// 9 will be Freshman, 10 Sophomore, 11 Junior, 12 Senior
-// Anything other than these values will return "Error" to the console
-
-
-
-
-
-// write a for loop that will iterate backwards from 10 to -10
-
-
-
-// write a do/while loop that prints 1 through 50
-
-
-// edit the previous do/while loop so that it prints the remainder when the loop number is divided by 4
-
-
-// Someone messed up the following for loop
-// fix the following infinite loop, uncomment to test
-/* 
-    for(let i = 11; i > 10; i++){
-        console.log(i);
-    } 
-*/
-
-
-
-
-
